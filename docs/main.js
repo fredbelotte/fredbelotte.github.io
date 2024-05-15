9610ab0b7cc2eba11fa250439c22c28fd3643dbb
fred belotte <fredbelotte@users.noreply.github.com>
2024-05-15 00:10:45 -0500

    set bulma, vue to automatic minor and patch version bump

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3.4.27/dist/vue.esm-browser.prod.js';
+import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.js';
 
 const socialLinks = ['GitHub', 'Linkedin'];
 
