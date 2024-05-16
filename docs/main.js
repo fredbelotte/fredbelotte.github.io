ffd7b3b3318d1da0025c964205415756588104c4
fred belotte <fredbelotte@users.noreply.github.com>
2024-05-15 23:40:56 -0500

    add fontawesome icon framework

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,22 @@
 import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.js';
 
-const socialLinks = ['GitHub', 'Linkedin'];
+const socialNetworks = [
+  {
+    icon: 'fa-brands fa-github',
+    text: 'GitHub',
+    url: 'https://github.com/fredbelotte/',
+  },
+  {
+    icon: 'fa-brands fa-linkedin',
+    text: 'Linkedin',
+    url: 'https://www.linkedin.com/in/fredbelotte/',
+  },
+];
 
 createApp({
   data() {
     return {
-      socialLinks,
+      socialNetworks,
     };
   },
 }).mount('#app');
